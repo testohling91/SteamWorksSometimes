8629d902ceb14e908bc12b158b6f3fb186d5f122
Tim Ohling <trohling@peak.org>
2017-01-24 19:18:19 -0800

    added required subsystem

--- SteamWorksSometimes/src/org/usfirst/frc/team4662/robot/commands/ArcadeDrive.java
+++ SteamWorksSometimes/src/org/usfirst/frc/team4662/robot/commands/ArcadeDrive.java
@@ -12,6 +12,7 @@ public class ArcadeDrive extends Command {
     public ArcadeDrive() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	requires(Robot.driveSystem);
     }
 
     // Called just before this Command runs the first time
